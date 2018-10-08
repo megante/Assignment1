9760a2518fd5e06286736e2e2b870598442f5b80
Megija <megi0005@edu.easj.dk>
2018-10-08 12:54:38 +0200

    mimi

--- Assignment1/Class1.cs
+++ Assignment1/Class1.cs
@@ -8,5 +8,14 @@ namespace Assignment1
 {
     public class Class1
     {
+        public double ConvertToGrams(double x)
+        {
+            return x * 28.3495231;
+        }
+
+        public double ConvertToOunces(double x)
+        {
+            return x * 0.0352739619;
+        }
     }
 }
---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Assignment1;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Class1 obj = new Class1();
+
+            double expected1 = 85.0485693;
+            double real1 = obj.ConvertToGrams(3);
+            double delta1 = 0.0005693;
+
+            Assert.AreEqual(expected1, real1, delta1);
+
+
+            double expected2 = 0.1058218857;
+            double real2 = obj.ConvertToOunces(3);
+            double delta2 = 0.0000218857;
+
+            Assert.AreEqual(expected2, real2, delta2);
+        }
+    }
+}
